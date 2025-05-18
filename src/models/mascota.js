0cd3e0efa1fbffc6312218e3aa7ef077f616dd52
Ronal Poveda <ronalpoveda03@gmail.com>
2025-05-18 16:25:45 -0500

    Adding
    adding content in mascota.js and adding index an mascota routes

---
+++ src/models/mascota.js
@@ -0,0 +1,20 @@
+// models/Mascota.js
+const mongoose = require('mongoose');
+
+const mascotaSchema = new mongoose.Schema({
+  nombre: { type: String, required: true },
+  tipo: {
+    type: String,
+    enum: ['perro', 'gato', 'zorro', 'pinguino'], // los 4 tipos válidos
+    required: true
+  },
+  bienestar: {
+    hambre: { type: Number, default: 100, min: 0, max: 100 },
+    energia: { type: Number, default: 100, min: 0, max: 100 },
+    diversion: { type: Number, default: 100, min: 0, max: 100 },
+  },
+  usuarioId: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
+  creadoEn: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Mascota', mascotaSchema);
