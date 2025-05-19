0aa7d285ec294eb29f61cdbc3b7d34f59e32e749
Ruzky <ruizgonzalezjulianarley25@gmail.com>
2025-05-18 22:36:03 -0500

    Index fixed
    Fixed an error that the index had

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ const parser = require("body-parser");
 const express = require('express');
 const app = express();
 const port = 3000;
-const mascotaRoutes = require("./routes/mascotaRoutes");
 const mongoose = require("mongoose");
+
+const mascotaRoutes = require("./routes/mascotaRoutes");
 const userRoutes = require("./routes/userRoutes");
+const acciones = require("./routes/accionesRoutes");
+const historial = require("./routes/historialRoutes");
+
 require('dotenv').config();
 app.use(parser.urlencoded({ extended: false })); //permite leer los datos que vienen en la petición
 app.use(parser.json()); // transforma los datos a formato JSON
 //Gestión de las rutas usando el middleware
 app.use("/api", mascotaRoutes);
 app.use("/api", userRoutes)
+app.use("/api", acciones)
+app.use("/api", historial)
 app.use(express.json());
 
 //Conexión a la base de datos
