4af4137dcc67d979921cc5a7ee27c44cf19801e2
Ruzky <ruizgonzalezjulianarley25@gmail.com>
2025-05-18 22:32:27 -0500

    Pet logic
    Added missing logic for pet operation

--- src/models/mascota.js
+++ src/models/mascota.js
@@ -1,17 +1,17 @@
-// models/Mascota.js
+// models/mascota.js
 const mongoose = require('mongoose');
 
 const mascotaSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
   tipo: {
     type: String,
-    enum: ['perro', 'gato', 'zorro', 'pinguino'], // los 4 tipos válidos
+    enum: ['perro', 'gato', 'zorro', 'pinguino'],
     required: true
   },
   bienestar: {
     hambre: { type: Number, default: 100, min: 0, max: 100 },
     energia: { type: Number, default: 100, min: 0, max: 100 },
-    diversion: { type: Number, default: 100, min: 0, max: 100 },
+    diversion: { type: Number, default: 100, min: 0, max: 100 }
   },
   usuarioId: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
   creadoEn: { type: Date, default: Date.now }
